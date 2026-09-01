fbea78b730b2da5d3055525d0eac176926648623
aliakbarr474 <aliakbarr474@gmail.com>
2026-09-02 00:12:39 +0500

    Fix puppeteer executable path for Linux

--- server.js
+++ server.js
@@ -45,8 +45,7 @@ const whatsappClient = new Client({
     remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
   },
   puppeteer: {
-    headless: true, 
-    executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+    headless: true,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
