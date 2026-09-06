85b890df0c1cacb16a3cc85bbcd52bb3fb8018d5
aliakbarr474 <aliakbarr474@gmail.com>
2026-09-06 18:47:22 +0500

    fixing stupidity

--- server.js
+++ server.js
@@ -86,6 +86,7 @@ process.on('unhandledRejection', (reason, promise) => {
 whatsappClient.initialize();
 
 const verifyToken = (req, res, next) => {
+    next();
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });
